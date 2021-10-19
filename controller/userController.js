ec370f2ee0b5455f6afc3fc1a1eb358a2799f193
arifrahman9 <arifrhmn910@gmail.com>
2021-10-19 13:34:25 +0700

    finish create feature login, authentication, and setup app.js

--- controller/userController.js
+++ controller/userController.js
@@ -13,6 +13,46 @@ class UserController {
       next(err)
     }
   }
+
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body
+      if (!email) {
+        throw { name: "Email is empty" }
+      }
+      if (!password) {
+        throw { name: "Password is empty" }
+      }
+      const response = await User.findOne({
+        where: {
+          email,
+        },
+      })
+
+      if (!response.email) {
+        throw { name: "Invalid Email" }
+      }
+
+      if (!comparePassword(password, response.password)) {
+        throw { name: "Invalid Password" }
+      }
+
+      if (response.status === "pending") {
+        throw { name: "Waiting Approved" }
+      }
+
+      const payload = {
+        id: response.id,
+        email: response.email,
+      }
+
+      const access_token = createToken(payload)
+
+      res.status(200).json({ access_token })
+    } catch (err) {
+      next(err)
+    }
+  }
 }
 
 module.exports = UserController
